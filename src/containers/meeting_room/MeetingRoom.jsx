8204d388e3c63d990aaf35cf3042c2bd8aeeec1e
vignesh-sellaperumal <60808963+vignesh-sellaperumal@users.noreply.github.com>
2024-03-05 22:32:51 +0530

    updated stun server url

--- src/containers/meeting_room/MeetingRoom.jsx
+++ src/containers/meeting_room/MeetingRoom.jsx
@@ -60,7 +60,7 @@ function MeetingRoom() {
     peer = new RTCPeerConnection({
       iceServers: [
         {
-          urls: "stun:stun.stunprotocol.org",
+          urls: "stun:stun.l.google.com:19302",
         },
         {
           urls: "turn:numb.viagenie.ca",
